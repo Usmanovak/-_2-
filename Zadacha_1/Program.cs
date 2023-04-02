24378691fec0387d14dc98e38422803f94a4e6ef
Karina <usmkarin@gmail.com>
2023-04-02 15:43:53 +0700

    Задача 1

---
+++ Zadacha_1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+//Console.WriteLine("Введите трехзначное число: ");
+int num = new Random().Next(100, 1000);
+int a1 = (num / 100); // 1 chislo
+int a2 = (num / 10 % 10); // 2 chislo
+int a3 = (num % 100 % 10); // 3
+Console.WriteLine(num);
+Console.Write(a1);
+Console.Write(a3);
