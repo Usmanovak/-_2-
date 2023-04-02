0321a1da547fcec852d22c434dcb684604d37c3b
Karina <usmkarin@gmail.com>
2023-04-02 16:17:43 +0700

    Решение задачи 2

--- Zadacha_2/Program.cs
+++ Zadacha_2/Program.cs
@@ -10,11 +10,4 @@ if (num1 % num2 == 0) {
     Console.WriteLine($"Остаток от деления: {num1 % num2}");
 
 
-// int num1 = new Random().Next(1, 100);
-// int num2 = new Random().Next(1, 100);
-// int result = num2 / num1;
-// if (num2 % num1 == 0) 
-// {
-//     Console.WriteLine($"Число {num2} кратно числу {num1}");
-// } else 
-//     Console.WriteLine($"Остаток от деления числа {num2} на {num1} равно: {result} ");
+
