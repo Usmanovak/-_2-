6277b0a8b3a8ec5e0bc809378becc4f40ec50ef5
Karina <usmkarin@gmail.com>
2023-04-02 16:09:40 +0700

    Задача 2

---
+++ Zadacha_2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 % num2 == 0) {
+    Console.WriteLine("Кратно");
+} else
+    Console.WriteLine($"Остаток от деления: {num1 % num2}");
+
+
+// int num1 = new Random().Next(1, 100);
+// int num2 = new Random().Next(1, 100);
+// int result = num2 / num1;
+// if (num2 % num1 == 0) 
+// {
+//     Console.WriteLine($"Число {num2} кратно числу {num1}");
+// } else 
+//     Console.WriteLine($"Остаток от деления числа {num2} на {num1} равно: {result} ");
