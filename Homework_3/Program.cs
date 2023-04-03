254986805a44ac18fee76cf3a04247ecf4cd4255
Karina <usmkarin@gmail.com>
2023-04-03 11:39:22 +0700

    Задача 3 из ДЗ

---
+++ Homework_3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Clear();
+Console.WriteLine("Ведите цифру дня недели: ");
+int num = int.Parse(Console.ReadLine());
+if ((num == 6) || (num == 7)) {
+    Console.WriteLine("Выходной");
+} else if (num < 6) 
+{
+    Console.WriteLine("Будний день");
+}
